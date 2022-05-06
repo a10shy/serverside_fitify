d90c1edebb7927c2ae343c32d8317dce67334300
atenshy <aloyisebiju2013@gmail.com>
2022-05-07 05:29:44 +0530

    first commit

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-var express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+var express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+var bodyParser = require('body-parser');  
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var app = express();
 
- 
-app.use(cors())
-
-const friendList = [
-   {
-      id: '1',   
-      name: 'Locke',
-      level: 17,
- },
- {
-   id: '2',   
-   name: 'Locke',
-   level: 17,
-},
-]; 
+app.use(cors());
+app.use(bodyParser.json());
 
 
-app.get('/get-friends/:id', function (req, res) {
-   const id = req.params.id;
+app.get("/get-friends/:id", function (req, res) {
+  const id = req.params.id;
+
+  fs.readFile("friendslist.json", "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+     const friendData = JSON.parse(data.toString());
+    console.log(friendData);
+    res.send(friendData);
+  });
+});
+
+app.post("/get-online", urlencodedParser, (req, res) => {
    
-   res.send(friendList);
-      
+ 
+   // var data = req;
+   console.log(req.body);
+   fs.appendFile("online.json", "[" + JSON.stringify(req.body) + "]", (err) => {
+      // console.log(err);
    })
-
-  
+   res.sendStatus(201)
+});
 
 var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+});
